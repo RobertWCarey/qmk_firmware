e1eb223aa1f6ed247df5987f5da4cf63ae1f6bc4
Rob Carey <rob_carey_1@hotmail.com>
2025-03-26 08:27:09 +1100

    Added back pointer layer logic and added handedness detection
    Did not bring in the extra auto swap and sniping behviour as that does
    appear to get enabled on the 3x6 layout so I have gotten used to not
    having it.

--- keyboards/bastardkb/charybdis/3x5/keymaps/rob/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/rob/keymap.c
@@ -21,12 +21,13 @@ enum charybdis_keymap_layers {
     LAYER_BASE = 0,
     LAYER_LOWER,
     LAYER_RAISE,
+    LAYER_POINTER,
 };
 
 #define LOWER MO(LAYER_LOWER)
 #define RAISE MO(LAYER_RAISE)
-// #define PT_Z LT(LAYER_POINTER, KC_Z)
-// #define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
+#define PT_Z LT(LAYER_POINTER, KC_Z)
+#define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
 
 #define HT_HOME_END LT(0, KC_HOME)
 #define LOW_ENT LT(LAYER_LOWER, KC_ENT)
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────────────┤
        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,      KC_H,    RSFT_T(KC_J),  RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN),
   // ├──────────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────────────┤
-       KC_Z,         KC_X,         KC_C,          KC_V,    KC_B,          KC_N,    KC_M,          KC_COMM,      KC_DOT,       KC_SLSH,
+       PT_Z,         KC_X,         KC_C,          KC_V,    KC_B,          KC_N,    KC_M,          KC_COMM,      KC_DOT,       PT_SLSH,
   // ╰──────────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────────────╯
                              KC_TAB, KC_SPC, LT(LAYER_RAISE, KC_ESC),     LOW_ENT, KC_BSPC
   //                               ╰────────────────────────────────────╯ ╰──────────────────╯
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────────────────────┤ ├──────────────────────────────────────────┤
           KC_0,     KC_1,    KC_2,    KC_3, KC_PSCR,   KC_TRNS,  EE_CLR, KC_TRNS, KC_TRNS, KC_NO,
   // ╰───────────────────────────────────────────────┤ ├──────────────────────────────────────────╯
-                           KC_TRNS, KC_TRNS, KC_TRNS,    KC_NO, QK_BOOT
+                           KC_TRNS, KC_TRNS, KC_TRNS,    KC_NO, KC_NO
   //                     ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -65,21 +66,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
          KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,    KC_UNDS,  KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
   // ╰──────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                          QK_BOOT, KC_TRNS,   KC_NO,    KC_TRNS, KC_TRNS
+                          KC_NO,  KC_TRNS,   KC_NO,    KC_TRNS, KC_TRNS
   //                    ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
-  // [LAYER_POINTER] = LAYOUT(
-  // // ╭───────────────────────────────────────────────╮ ╭──────────────────────────────────────────────╮
-  //      KC_NO,   KC_NO,   KC_NO,   DPI_MOD, S_D_MOD,      S_D_MOD,  DPI_MOD, KC_NO,   KC_NO,   KC_NO,
-  // // ├───────────────────────────────────────────────┤ ├──────────────────────────────────────────────┤
-  //      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,        KC_NO,    KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
-  // // ├───────────────────────────────────────────────┤ ├──────────────────────────────────────────────┤
-  //      KC_TRNS, DRGSCRL, KC_COPY, KC_PSTE, QK_BOOT,      QK_BOOT,  EE_CLR,  SNIPING, DRGSCRL, KC_TRNS,
-  // // ╰───────────────────────────────────────────────┤ ├──────────────────────────────────────────────╯
-  //                          KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1
-  // //                     ╰───────────────────────────╯ ╰──────────────────╯
-  // ),
+  [LAYER_POINTER] = LAYOUT(
+  // ╭───────────────────────────────────────────────╮ ╭──────────────────────────────────────────────╮
+       KC_NO,   KC_NO,   KC_NO,   DPI_MOD, S_D_MOD,      S_D_MOD,  DPI_MOD, KC_NO,   KC_NO,   KC_NO,
+  // ├───────────────────────────────────────────────┤ ├──────────────────────────────────────────────┤
+       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,        KC_NO,    KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
+  // ├───────────────────────────────────────────────┤ ├──────────────────────────────────────────────┤
+       KC_TRNS, DRGSCRL, KC_COPY, KC_PSTE, QK_BOOT,      QK_BOOT,  EE_CLR,  SNIPING, DRGSCRL, KC_TRNS,
+  // ╰───────────────────────────────────────────────┤ ├──────────────────────────────────────────────╯
+                           KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1
+  //                     ╰───────────────────────────╯ ╰──────────────────╯
+  ),
 };
 // clang-format on
 
@@ -117,4 +118,3 @@ bool caps_word_press_user(uint16_t keycode) {
             return false; // Deactivate Caps Word.
     }
 }
-
