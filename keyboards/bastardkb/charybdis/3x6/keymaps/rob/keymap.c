d488c26007c42ff2537c184e645417710f2a44ef
Rob Carey <rob_carey_1@hotmail.com>
2025-04-01 08:52:32 +1100

    Updated keymap to make GUI layer shift more permissive
    Change the lower layer to have KC_TRNS instead of KC_NO. This means that
    you need to press the GUI key first otherwise the intended sequence.

--- keyboards/bastardkb/charybdis/3x6/keymaps/rob/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/rob/keymap.c
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        KC_NO, HT_HOME_END, KC_7,    KC_8,    KC_9, KC_NO,     KC_TILD, KC_DQUO, KC_QUOT, KC_GRV,  KC_DEL, KC_NO,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_NO,    KC_NO,    KC_4,    KC_5,    KC_6, KC_NO,     KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, KC_NO, KC_NO,
+       KC_NO,    KC_NO,    KC_4,    KC_5,    KC_6, KC_NO,     KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, KC_TRNS, KC_NO,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_NO,    KC_0,     KC_1,    KC_2,    KC_3, KC_PSCR,   KC_TRNS,  EE_CLR, KC_TRNS, KC_TRNS, KC_NO, KC_NO,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_TRNS, KC_TRNS, KC_TRNS,    KC_NO, QK_BOOT
+                                  KC_TRNS, KC_TRNS, KC_TRNS,    KC_NO, KC_NO
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_NO,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,    KC_UNDS,  KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_NO,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  QK_BOOT, KC_TRNS,   KC_NO,    KC_TRNS, KC_TRNS
+                                  KC_NO , KC_TRNS,   KC_NO,    KC_TRNS, KC_TRNS
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
